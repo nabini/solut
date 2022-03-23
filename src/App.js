f95c0e813703c7460c73dac02db59dbd4e6f1f2e
Kone <kone@gmail.com>
2022-03-23 19:08:05 +0100

    nabini

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={require("./images/m.jpg")} alt="" />
+      <img src={require("./images/m1.jpg")} alt="" />
+      <img src={require("./images/m2.jpg")} alt="" />
+    </div>
+  );
+}
+
+export default App;
